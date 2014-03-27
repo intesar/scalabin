e6ff1cc1f50a133b46fb2101e96997885d23450d
Sachin Modak <sachin.modak@gmail.com>
2014-03-27 14:18:41 -0700

    Config file changes environment.

--- src/main/java/com/vmw/bora/vchest/config/SolrContext.java
+++ src/main/java/com/vmw/bora/vchest/config/SolrContext.java
@@ -24,26 +24,33 @@ public class SolrContext {
 
     /*
     Schema.xml content.
-    [3/27/14 10:24:53 AM] Intesar Mohammed: <field name="username" type="text_general" indexed="true" stored="true"/>
-   <field name="joinDate" type="date" indexed="true" stored="true"/>
-   <field name="storageSize" type="float" indexed="true" stored="true"/>
-[3/27/14 12:32:06 PM] sachin modak:     <field name="userName" type="text_general" indexed="true" stored="true"/>
+    <field name="username" type="text_general" indexed="true" stored="true"/>
+    <field name="joinDate" type="date" indexed="true" stored="true"/>
+    <field name="storageSize" type="float" indexed="true" stored="true"/>
+
     <field name="enabled" type="boolean" indexed="true" stored="true"/>
     <field name="tenantId" type="text_general" indexed="true" stored="true"/>
-[3/27/14 12:32:40 PM] Omkar Raut: <!-- authority -->
-   <field name="authority" type="text_general" indexed="true" stored="true"/>
-   
-   <!-- stats -->
-   <field name="user" type="text_general" indexed="true" stored="true"/>
-   <field name="year" type="int" indexed="true" stored="true"/>
-   <field name="month" type="int" indexed="true" stored="true"/>
-   <field name="storage" type="long" indexed="true" stored="true"/>
-   <field name="uploadedBytes" type="long" indexed="true" stored="true"/>
-   <field name="downloadedBytes" type="long" indexed="true" stored="true"/>
-[3/27/14 1:47:55 PM] Omkar Raut: <!-- activity -->
-    <field name="date" type="date" indexed="true" stored="true"/>
+
+    <field name="bucketName" type="text_general" indexed="true" stored="true"/>
+    <field name="kind" type="text_general" indexed="true" stored="true"/>
+    <field name="locationUri" type="text_general" indexed="true" stored="true"/>
+    <field name="size" type="text_general" indexed="true" stored="true"/>
+    <field name="parent" type="text_general" indexed="true" stored="true"/>
+    <field name="dateModified" type="text_general" indexed="true" stored="true"/>
+    <field name="chunkCount" type="text_general" indexed="true" stored="true"/>
+    <field name="owner" type="text_general" indexed="true" stored="true"/>
+    <field name="tenant" type="text_general" indexed="true" stored="true"/>
+    <field name="group" type="text_general" indexed="true" stored="true"/>
+    <field name="shared" type="text_general" indexed="true" stored="true"/>
+    <field name="authority" type="text_general" indexed="true" stored="true"/>
+    <field name="user" type="text_general" indexed="true" stored="true"/>
+    <field name="year" type="text_general" indexed="true" stored="true"/>
+    <field name="month" type="text_general" indexed="true" stored="true"/>
+    <field name="storage" type="text_general" indexed="true" stored="true"/>
+    <field name="uploadedBytes" type="text_general" indexed="true" stored="true"/>
+    <field name="downloadedBytes" type="text_general" indexed="true" stored="true"/>
+    <field name="date" type="text_general" indexed="true" stored="true"/>
     <field name="activity" type="text_general" indexed="true" stored="true"/>
-	<field name="objId" type="int" indexed="true" stored="true"/>
-	<field name="size" type="long" indexed="true" stored="true"/>
+	<field name="objId" type="text_general" indexed="true" stored="true"/>
     */
 }
